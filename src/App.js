7968efa709c217cdb804e7665082675dc5ca2ca5
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-03-12 14:04:27 -0300

    arrumando erro de lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/" component={ Wallet } />
-    </Switch>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/carteira" component={ Wallet } />
+        </Switch>
+      </main>
+    );
+  }
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/store';
 
 ReactDOM.render(
   <BrowserRouter>
