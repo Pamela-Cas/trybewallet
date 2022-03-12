ca6c9c9f087e59b7c8aae213226543b52946805c
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-03-12 13:56:56 -0300

    alterando arquivo

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,39 +1,71 @@
-import { FAILED_REQUEST, GET_INPUTFORM, GET_MONEY, REQUEST_API } from '../actions';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const INITIAL_STATE = {
-  currencies: [],
-  expenses: [],
-  error: 'Erro',
-  exchangeRates: [],
-};
+class Wallet extends React.Component {
+  constructor() {
+    super();
 
-const wallet = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-  case REQUEST_API:
-    return {
-      ...state,
+    this.state = {
+      spent: 0,
     };
-  case GET_MONEY:
-    return {
-      ...state,
-      currencies: Object.keys(action.payload)
-        .filter((money) => money !== 'USDT'),
-      exchangeRates: action.payload,
-    };
-  case FAILED_REQUEST:
-    return {
-      ...state,
-      error: action.error,
-    };
-  case GET_INPUTFORM:
-    return {
-      ...state,
-      expenses: action.payload,
-      exchangeRates: action.payload,
-    };
-  default:
-    return state;
   }
-};
 
-export default wallet;
+  render() {
+    const { userEmail } = this.props;
+    const { spent } = this.state;
+    return (
+      <header>
+        <h3 data-testid="email-field">{ userEmail }</h3>
+        <form>
+          <label htmlFor="money-by" data-testid="total-field">
+            Valor:
+            <input
+              type="number"
+              id="money-by"
+            />
+            { spent }
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select data-testid="header-currency-field" id="moeda">
+              <option>BRL</option>
+            </select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select id="pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="descrição">
+            Descrição:
+            <input type="text" id="descrição" />
+          </label>
+        </form>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
